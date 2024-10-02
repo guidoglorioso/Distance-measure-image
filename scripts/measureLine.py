3b7703d1769ce6455236bfccfa80865e9fafa1be
guidoglorioso <guido.glorioso@gmail.com>
2024-10-02 01:03:56 -0300

    Documento codigo

--- scripts/measureLine.py
+++ scripts/measureLine.py
@@ -44,7 +44,8 @@ def filtro_color(img : np.array, color_filtro : str) -> np.array:
     Returns:
         np.array: Imagen filtrada
     """
-    if not check_color(color_filtro): return 0
+    # Verifico si el color es valido
+    check_color(color_filtro)
 
     _img = img.copy()
 
@@ -86,27 +87,28 @@ def detectar_referencia_px(img : np.array, color_ref : str = "verde",ref :float 
     Es necesario que las lineas tengan la misma distancia y una encima de la otra en forma paralela (cualquier distancia entre lineas). En caso tipico 3 lineas de 5cm funciona correcto .
 
     Args:
-        img (np.array): imagen
+        img (np.array): imagen RGB
         color_ref (str, optional): Color a detectar como referencia. Defaults to "verde".
         ref (float, optional): largo de linea en cm. Defaults to 5.
 
     Returns:
-        tuple: (unidades pixel, imagen rotada)
+        tuple: (unidades pixel, imagen RGB rotada)
     """
 
     _img = img.copy()
 
     # Aplico filtro referencia
     _img2 = filtro_color(_img, color_ref)
-   
+
+
     # Obtengo bordes de lineas
     _img2 = cv2.Canny(_img2,  threshold1=50,  threshold2=60, apertureSize=3)
-
+    
     # Detecto lineas
-    _lineas = cv2.HoughLinesP(_img2, rho=1, theta=np.pi/180, threshold=200, minLineLength=180, maxLineGap=30)
-    
-    if len(_lineas) == 0:
-    
+    _lineas = cv2.HoughLinesP(_img2, rho=1, theta=np.pi/360, threshold=200, minLineLength=180, maxLineGap=30)
+
+
+    if _lineas is None:
         raise ValueError(f"La imagen no posee referencia adecuada")
     
     else:
@@ -137,53 +139,124 @@ def detectar_referencia_px(img : np.array, color_ref : str = "verde",ref :float 
     _largos = np.array([np.sqrt((_x2 - _x1)**2 + (y2 - y1)**2) for linea in _lineas for _, y1, _, y2 in linea]) 
     
     # Calculo medicion de un pixel
+
     unidad_px = ref / _largos.mean()
+
 
     return unidad_px,imagen_rotada
 
 def buscar_centro(imagen:np.array ,color_ref:str = "amarillo") -> np.array:
+    """Detecta un color en una imagen (color_ref) y en base a todos los pixeles de este color obtiene el punto medio de todos.
 
-    filtro_ =  filtro_color(imagen,color_ref)
+    Args:
+        imagen (np.array): imagen RGB 
+        color_ref (str, optional): color a detectar. Defaults to "amarillo".
 
-    coordenadas_negra = np.argwhere(cv2.bitwise_not(filtro_amarillo))
-    promedio = np.round(coordenadas_negra.mean(axis=0))
+    Returns:
+        np.array: Coordenadas (x,y) del centro del color
+    """
+    _filtro =  filtro_color(imagen,color_ref)
+
+    _coord = np.argwhere(cv2.bitwise_not(_filtro))
+    promedio = np.round(_coord.mean(axis=0))
+
     return promedio[0:2][::-1]
 
-def trasladar_imagen_referencia(imagen, coord_ref, coord_dest, dim_out=0):
+def trasladar_imagen_referencia(imagen:np.array, coord_ref:tuple, coord_dest:tuple)-> np.array:
+    """Traslada una imagen respecto a un centro dado (coord_ref) a un segundo centro (coord_dest)
+
+    Args:
+        imagen (np.array): imagen RGB a trasladar
+        coord_ref (tuple): coordenadas origen
+        coord_dest (tuple): coordenadas destino
+
+    Returns:
+        np.array: imagen RGB trasladada
+    """
+
     # Cargar la imagen original
     _imagen = imagen.copy()
     
     # Obtener las dimensiones de la imagen
    
-    alto, ancho = _imagen.shape[:2]
+    _alto, _ancho = _imagen.shape[:2]
+
+    # Calculo delta de coords
         
     _desp_x =   coord_dest[0] - coord_ref[0]
     _desp_y =   coord_dest[1]  - coord_ref[1]
     
     # Crear una matriz de transformación (traslación)
-    M = np.float32([[1, 0, _desp_x], [0, 1, _desp_y]])
+    _M = np.float32([[1, 0, _desp_x], [0, 1, _desp_y]])
     
     # Aplicar la transformación de traslación
-    imagen_trasladada = cv2.warpAffine(imagen, M, (ancho, alto), borderValue=(255, 255, 255)) # Rellena con blanco
-    if dim_out > 0:
-        imagen_trasladada =  imagen_trasladada[0:dim_out,0:dim_out]
-    return imagen_trasladada
+    _imagen_trasladada = cv2.warpAffine(imagen, _M, (_ancho, _alto), borderValue=(255, 255, 255)) # Rellena con blanco
+    
+    return _imagen_trasladada
 
-def resize_y_threshold(imagen, nuevo_ancho, nuevo_alto, threshold=127):
-    # Hacer un resize de la imagen a 200x200 usando el método de INTER_AREA (adecuado para reducir)
-    imagen_redimensionada = cv2.resize(imagen, (nuevo_ancho, nuevo_alto), interpolation=cv2.INTER_AREA)
-    imagen_redimensionada = cv2.bitwise_not(imagen_redimensionada)
+def resize_y_threshold(imagen:np.array, nueva_dim : tuple, threshold:int=127)-> np.array:
+    """Toma una imagen y la redimenciona la imagen achicando a nueva_dim pixeles (ancho,alto) y binariza segun el umbral seteado. Todo lo que supere el umbral tendra un pixel negro
+
+    Args:
+        imagen (np.array): imagen RGB
+        nueva_dim (tuple): dimensiones nuevas de la imagen (nuevo_ancho,nuevo_alto)
+        threshold (int, optional): valor umbral para detecta como pixel negro (cuanto mayor umbral los pixeles tienen que ser menos negros). Defaults to 127.
+
+    Returns:
+        np.array: imagen Grey binarizada.
+
+    """
+    _img = imagen.copy()
+
+    # Se hace un resize promediando los pixeles vecinos
+    _img = cv2.resize(_img, nueva_dim, interpolation=cv2.INTER_AREA)
+    
     # Aplicar el threshold: si el valor promedio del píxel supera el threshold, se pone negro (0)
     # Sino, se pone blanco (255)
-    _, imagen_binarizada = cv2.threshold(imagen_redimensionada, threshold, 255, cv2.THRESH_BINARY_INV)
-    
+    _img = cv2.cvtColor(_img,cv2.COLOR_RGB2GRAY)
+
+    _, imagen_binarizada = cv2.threshold(_img, threshold, 255, cv2.THRESH_BINARY)
+
+
     return imagen_binarizada
 
-def crop_img(img,centro, pxl_dim):
-    
-    dim = [pxl_dim[0],pxl_dim[1],3]
+def grey2rgb(img:np.array):
+    return cv2.cvtColor(img,cv2.COLOR_GRAY2RGB)
 
-    _ret = np.ones(dim,dtype=np.uint8)*255
+
+def crop_img(img:np.array ,centro:tuple, pxl_dim:tuple, offset:int = 50):
+    """
+    Recorta una imagen dada centrada en un punto específico y con dimensiones determinadas.
+    
+    Esta función toma una imagen `img` y recorta una sección centrada en las coordenadas 
+    `centro`, con dimensiones especificadas por `pxl_dim`. Si el recorte se extiende 
+    más allá de los límites de la imagen, la función llena el área sobrante con píxeles blancos.
+
+    Parámetros:
+    ----------
+    img : np.array
+        La imagen de entrada representada como un arreglo de NumPy con formato (alto, ancho, canales).
+    
+    centro : tuple
+        Una tupla (x, y) que define el centro del área a recortar en la imagen.
+    
+    pxl_dim : tuple
+        Una tupla (alto, ancho) que define las dimensiones (en píxeles) del área a recortar.
+
+    offset: int 
+        Cantidad de pixeles para desplazar en Y el centro.
+    
+    Retorna:
+    -------
+    np.array
+        Una nueva imagen del tamaño especificado por `pxl_dim`, con el área recortada centrada
+        en las coordenadas `centro`. Si las dimensiones exceden los límites de la imagen original,
+        las áreas vacías se rellenan con píxeles blancos.
+    """
+    
+    _dim = [pxl_dim[0],pxl_dim[1],3]
+
+    _ret = np.ones(_dim,dtype=np.uint8)*255
 
     pxl_x_inicial =  max(0, int(centro[0] - pxl_dim[0] // 2))
     pxl_x_final   =  min(img.shape[0], int(centro[0] + pxl_dim[0]//2))
@@ -198,26 +271,28 @@ def crop_img(img,centro, pxl_dim):
     return _ret
 
 
-def get_matriz(ruta_img):
+def get_matriz(ruta_img:str,dim_cuadrado:float,dim_rejilla:float)->np.array:
+    """Funcion que recibe una ruta de imagen y obtiene de ella una matriz reducida donde cada pixel representa una distancia "dim_cuadrado" 
+    y en donde la cantidad total de pixeles depende de la dimension que se desea representar en total "dim_rejilla"\n
+    Por defecto se toma como referencia de medida el color "verde", como centro el color "amarillo" y como objeto el color "negro".
 
+    Args:
+        ruta_img (str): ruta imagen
+        dim_cuadrado (float): medida de cada pixel en cm
+        dim_rejilla (float): medida de la imagen en cm
+
+    Returns:
+        np.array: imagen formato grey centrada en la referencia y escalada 
+    """
     # Levanto la imagen y la proceso en RGB
     _img = cv2.imread(ruta_img)
     _img = cv2.cvtColor(_img, cv2.COLOR_BGR2RGB)
-
-    # Detecto la referencia, obtengo medida de px y roto imagen segun referencia
-    try:
-        _px_dim, _img_rot = detectar_referencia_px(_img,"verde",ref=5) # Ref: medida en cm de la linea de referencia
-    except ValueError as e:
-        print(e)
-        return 0
     
+    # Detecto la referencia, obtengo medida de px y roto imagen segun referencia
+    _px_dim, _img_rot = detectar_referencia_px(_img,"verde",ref=5) # Ref: medida en cm de la linea de referencia
+   
     # Detecto referencia de centro
     _centro = buscar_centro(_img_rot)
-
-    # Defino rejilla
-    # Parametros rejilla:
-    dim_cuadrado=0.1 # Cm lado
-    dim_rejilla = 20 # Cm por lado
 
     # Calculo la cantidad de pixeles para la cuadricula.
     _cant_cuadrados = int(dim_rejilla / dim_cuadrado)
@@ -241,20 +316,35 @@ def get_matriz(ruta_img):
     _img = trasladar_imagen_referencia(_img, _centro, _new_centro)
 
     # Promedio los cuadrados de la rejilla para que solo tengan un PXL (bajo resolucion)
-    _resultado = resize_y_threshold(_img,200,200,threshold=50)
-
-    return _resultado
+    _resultado = resize_y_threshold(_img,(200,200),threshold=200)
 
 
-def generar_cuadrilla(imagen, separacion_px, color=(255, 0, 0)):
+
+    return grey2rgb(_resultado)
+
+
+
+def generar_cuadrilla(imagen:np.array, separacion_px:int, color : tuple=(255, 0, 0)):
+    """_summary_
+
+    Args:
+        imagen (np.array): imagen RGB.
+        separacion_px (int): separacion entre lineas
+        color (tuple, optional): Color de la cuadrilla. Defaults to (255, 0, 0).
+
+    Returns:
+        _type_: imagen con rejilla
+    """
+
     # Crear una imagen blanca del tamaño especificado
-    _imagen = imagen.copy()
+    _img = imagen.copy()
+
     ancho,alto = _img.shape[0:2]
     # Dibujar líneas horizontales y verticales con la separación deseada
     for x in range(0, ancho, separacion_px):
-        cv2.line(_imagen, (x, 0), (x, alto), color, 2)  # Líneas verticales
+        cv2.line(_img, (x, 0), (x, alto), color, 2)  # Líneas verticales
 
     for y in range(0, alto, separacion_px):
-        cv2.line(_imagen, (0, y), (ancho, y), color, 2)  # Líneas horizontales
+        cv2.line(_img, (0, y), (ancho, y), color, 2)  # Líneas horizontales
 
-    return _imagen
+    return _img
